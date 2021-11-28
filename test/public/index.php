60f7d85060e3f06846e9eb9f5bec96863d1abb8c
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2021-11-28 15:37:20 +0900

    "/hellow/:id" エンドポイント　追加

--- test/public/index.php
+++ test/public/index.php
@@ -1,78 +1,91 @@
 <?php
-declare(strict_types=1);
 
-use App\Application\Handlers\HttpErrorHandler;
-use App\Application\Handlers\ShutdownHandler;
-use App\Application\ResponseEmitter\ResponseEmitter;
-use App\Application\Settings\SettingsInterface;
-use DI\ContainerBuilder;
-use Slim\Factory\AppFactory;
-use Slim\Factory\ServerRequestCreatorFactory;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/../vendor/autoload.php';
+require '../vendor/autoload.php';
 
-// Instantiate PHP-DI ContainerBuilder
-$containerBuilder = new ContainerBuilder();
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+		$name = $args['name'];
+		$response->getBody()->write("Hello, $name");
+		return $response;
+});
 
-if (false) { // Should be set to true in production
-	$containerBuilder->enableCompilation(__DIR__ . '/../var/cache');
-}
+// declare(strict_types=1);
 
-// Set up settings
-$settings = require __DIR__ . '/../app/settings.php';
-$settings($containerBuilder);
+// use App\Application\Handlers\HttpErrorHandler;
+// use App\Application\Handlers\ShutdownHandler;
+// use App\Application\ResponseEmitter\ResponseEmitter;
+// use App\Application\Settings\SettingsInterface;
+// use DI\ContainerBuilder;
+// use Slim\Factory\AppFactory;
+// use Slim\Factory\ServerRequestCreatorFactory;
 
-// Set up dependencies
-$dependencies = require __DIR__ . '/../app/dependencies.php';
-$dependencies($containerBuilder);
+// require __DIR__ . '/../vendor/autoload.php';
 
-// Set up repositories
-$repositories = require __DIR__ . '/../app/repositories.php';
-$repositories($containerBuilder);
+// // Instantiate PHP-DI ContainerBuilder
+// $containerBuilder = new ContainerBuilder();
 
-// Build PHP-DI Container instance
-$container = $containerBuilder->build();
+// if (false) { // Should be set to true in production
+// 	$containerBuilder->enableCompilation(__DIR__ . '/../var/cache');
+// }
 
-// Instantiate the app
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-$callableResolver = $app->getCallableResolver();
+// // Set up settings
+// $settings = require __DIR__ . '/../app/settings.php';
+// $settings($containerBuilder);
 
-// Register middleware
-$middleware = require __DIR__ . '/../app/middleware.php';
-$middleware($app);
+// // Set up dependencies
+// $dependencies = require __DIR__ . '/../app/dependencies.php';
+// $dependencies($containerBuilder);
 
-// Register routes
-$routes = require __DIR__ . '/../app/routes.php';
-$routes($app);
+// // Set up repositories
+// $repositories = require __DIR__ . '/../app/repositories.php';
+// $repositories($containerBuilder);
 
-/** @var SettingsInterface $settings */
-$settings = $container->get(SettingsInterface::class);
+// // Build PHP-DI Container instance
+// $container = $containerBuilder->build();
 
-$displayErrorDetails = $settings->get('displayErrorDetails');
-$logError = $settings->get('logError');
-$logErrorDetails = $settings->get('logErrorDetails');
+// // Instantiate the app
+// AppFactory::setContainer($container);
+// $app = AppFactory::create();
+// $callableResolver = $app->getCallableResolver();
 
-// Create Request object from globals
-$serverRequestCreator = ServerRequestCreatorFactory::create();
-$request = $serverRequestCreator->createServerRequestFromGlobals();
+// // Register middleware
+// $middleware = require __DIR__ . '/../app/middleware.php';
+// $middleware($app);
 
-// Create Error Handler
-$responseFactory = $app->getResponseFactory();
-$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+// // Register routes
+// $routes = require __DIR__ . '/../app/routes.php';
+// $routes($app);
 
-// Create Shutdown Handler
-$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
-register_shutdown_function($shutdownHandler);
+// /** @var SettingsInterface $settings */
+// $settings = $container->get(SettingsInterface::class);
 
-// Add Routing Middleware
-$app->addRoutingMiddleware();
+// $displayErrorDetails = $settings->get('displayErrorDetails');
+// $logError = $settings->get('logError');
+// $logErrorDetails = $settings->get('logErrorDetails');
 
-// Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logError, $logErrorDetails);
-$errorMiddleware->setDefaultErrorHandler($errorHandler);
+// // Create Request object from globals
+// $serverRequestCreator = ServerRequestCreatorFactory::create();
+// $request = $serverRequestCreator->createServerRequestFromGlobals();
 
-// Run App & Emit Response
-$response = $app->handle($request);
-$responseEmitter = new ResponseEmitter();
-$responseEmitter->emit($response);
+// // Create Error Handler
+// $responseFactory = $app->getResponseFactory();
+// $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+
+// // Create Shutdown Handler
+// $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+// register_shutdown_function($shutdownHandler);
+
+// // Add Routing Middleware
+// $app->addRoutingMiddleware();
+
+// // Add Error Middleware
+// $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logError, $logErrorDetails);
+// $errorMiddleware->setDefaultErrorHandler($errorHandler);
+
+// // Run App & Emit Response
+// $response = $app->handle($request);
+// $responseEmitter = new ResponseEmitter();
+// $responseEmitter->emit($response);
